298a924df98b48b87f3e20549d698e83cbfde1b0
Rhys Tilford <rhystilford@gmail.com>
2023-09-28 22:30:18 -0400

    Day1 completed

--- Day1Part2.js
+++ Day1Part2.js
@@ -1,6 +1,10 @@
+
+
+// Extract the numbers from the text file.
 const fs = require('fs');
 const Input = fs.readFileSync('Day1Input.txt', 'utf8').split('\r\n');
 
+// Find the break points between each elf.
 function getAllIndexes(arr, val) {
     var indexes = [], i;
     for(i = 0; i < arr.length; i++)
@@ -9,7 +13,25 @@ function getAllIndexes(arr, val) {
     return indexes;
 }
 
-console.log(getAllIndexes(Input, ''));
+let indexes = getAllIndexes(Input, '');
+
+// Use the break points to generate an array of arrays of each elves' food items.
+var elves = [], i;
+for(i = 0; i < indexes.length; i++) {
+    elves[i] = Input.slice(indexes[i], indexes[i + 1]);
+}
+
+// Find an array of the sums of each of the elves' calories.
+let elfSums = elves.map((array) => array.reduce((a,b) => +a + +b));
+
+// Sort the arrays in descending order.
+elfSums.sort(function (a,b) {
+    return b - a;
+});
+
+// Find the number of calories for the elf with the highest calories
+console.log(elfSums[0]);
 
 
-
+// Find the sum of the first three elements.
+console.log(elfSums[0] + elfSums[1] + elfSums[2]);
